d6b61581535173d56635b51e8b7253ef08d079e0
Stephen Charlesworth <stephen.charlesworth@cookmedical.com>
2018-01-20 22:14:29 -0500

    successful deploy as lambda function

--- lambda_deploy/simple_lambda_update.py
+++ lambda_deploy/simple_lambda_update.py
@@ -1,5 +1,5 @@
 import boto3
-lambda_client = boto3.client('lambda')
+lambda_client = boto3.client('lambda', region_name = 'us-east-2')
 with open('PetBot.zip', 'rb') as f:
   zipped_code = f.read()
 lambda_client.update_function_code(
